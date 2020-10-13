78b620c7ea861254696bff52f07778fba39be007
KristofferEdstrom <kristoffer.edstrom92@gmail.com>
2020-10-13 17:00:39 +0200

    Last changes

---
+++ js/home.js
@@ -0,0 +1,111 @@
+img1 = document.getElementById("img1"); 
+// Function to set image dimensions 
+function enlargeImg1() {
+    img1.style.zIndex ="1";
+    img1.style.width = "300%"; 
+    img1.style.height = "auto"; 
+    img1.style.transition = "width 0.5s ease"; 
+} 
+// Function to reset image dimensions 
+function resetImg1() { 
+    img1.style.zIndex ="0";
+    img1.style.width = "100%"; 
+    img1.style.height = "auto"; 
+    img1.style.transition = "width 0.5s ease"; 
+} 
+
+img2 = document.getElementById("img2"); 
+// Function to set image dimensions 
+function enlargeImg2() {
+    img2.style.zIndex ="1";
+    img2.style.width = "300%"; 
+    img2.style.height = "auto"; 
+    img2.style.transition = "width 0.5s ease"; 
+} 
+// Function to reset image dimensions 
+function resetImg2() { 
+    img2.style.zIndex ="0";
+    img2.style.width = "100%"; 
+    img2.style.height = "auto"; 
+    img2.style.transition = "width 0.5s ease"; 
+} 
+
+img3 = document.getElementById("img3"); 
+// Function to set image dimensions 
+function enlargeImg3() {
+    img3.style.zIndex ="1";
+    img3.style.width = "300%"; 
+    img3.style.height = "auto"; 
+    img3.style.transition = "width 0.5s ease"; 
+} 
+// Function to reset image dimensions 
+function resetImg3() { 
+    img3.style.zIndex ="0";
+    img3.style.width = "100%"; 
+    img3.style.height = "auto"; 
+    img3.style.transition = "width 0.5s ease"; 
+} 
+
+img3 = document.getElementById("img3"); 
+// Function to set image dimensions 
+function enlargeImg3() {
+    img3.style.zIndex ="1";
+    img3.style.width = "300%"; 
+    img3.style.height = "auto"; 
+    img3.style.transition = "width 0.5s ease"; 
+} 
+// Function to reset image dimensions 
+function resetImg3() { 
+    img3.style.zIndex ="0";
+    img3.style.width = "100%"; 
+    img3.style.height = "auto"; 
+    img3.style.transition = "width 0.5s ease"; 
+} 
+
+img4 = document.getElementById("img4"); 
+// Function to set image dimensions 
+function enlargeImg4() {
+    img4.style.zIndex ="1";
+    img4.style.width = "300%"; 
+    img4.style.height = "auto"; 
+    img4.style.transition = "width 0.5s ease"; 
+} 
+// Function to reset image dimensions 
+function resetImg4() { 
+    img4.style.zIndex ="0";
+    img4.style.width = "100%"; 
+    img4.style.height = "auto"; 
+    img4.style.transition = "width 0.5s ease"; 
+} 
+
+img5 = document.getElementById("img5"); 
+// Function to set image dimensions 
+function enlargeImg5() {
+    img5.style.zIndex ="1";
+    img5.style.width = "300%"; 
+    img5.style.height = "auto"; 
+    img5.style.transition = "width 0.5s ease"; 
+} 
+// Function to reset image dimensions 
+function resetImg5() { 
+    img5.style.zIndex ="0";
+    img5.style.width = "100%"; 
+    img5.style.height = "auto"; 
+    img5.style.transition = "width 0.5s ease"; 
+} 
+
+img6 = document.getElementById("img6"); 
+// Function to set image dimensions 
+function enlargeImg6() {
+    img6.style.zIndex ="1";
+    img6.style.width = "300%"; 
+    img6.style.height = "auto"; 
+    img6.style.transition = "width 0.5s ease"; 
+} 
+// Function to reset image dimensions 
+function resetImg6() { 
+    img6.style.zIndex ="0";
+    img6.style.width = "100%"; 
+    img6.style.height = "auto"; 
+    img6.style.transition = "width 0.5s ease"; 
+} 
